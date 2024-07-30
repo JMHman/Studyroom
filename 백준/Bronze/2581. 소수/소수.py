6d4720a77f53c72224dfde149123e5642240511e
전명환 <153697827+JMHman@users.noreply.github.com>
2024-07-30 15:44:43 +0900

    [Bronze II] Title: 소수, Time: 52 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,25 @@
+import math
+
+# 소수 판별 함수
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# 입력 받기
+M = int(input())
+N = int(input())
+
+# M 이상 N 이하의 소수 찾기
+prime_list = [num for num in range(M, N + 1) if is_prime(num)]
+
+if prime_list:
+    prime_sum = sum(prime_list)
+    prime_min = min(prime_list)
+    print(prime_sum)
+    print(prime_min)
+else:
+    print(-1)
