fe4b9a8147aedbf6385b7b047114ff918613390a
전명환 <153697827+JMHman@users.noreply.github.com>
2024-07-30 16:52:58 +0900

    [Bronze I] Title: 소인수분해, Time: 168 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -5,19 +5,23 @@ const N = parseInt(fs.readFileSync('/dev/stdin').toString().trim(), 10);
 
 function primeFactors(n) {
     const factors = [];
-    let i = 2;
     
-    while (i * i <= n) { // i가 n의 제곱근까지 반복
-        if (n % i === 0) { // i로 나누어 떨어지면
-            while (n % i === 0) { // 계속 나누어떨어질 때까지
-                factors.push(i);
-                n /= i;
-            }
-        }
-        i++;
+    // 2로 나누기
+    while (n % 2 === 0) {
+        factors.push(2);
+        n /= 2;
     }
     
-    if (n > 1) { // n이 1보다 크면, n 자체가 소수
+    // 3 이상의 홀수로 나누기
+    for (let i = 3; i * i <= n; i += 2) {
+        while (n % i === 0) {
+            factors.push(i);
+            n /= i;
+        }
+    }
+    
+    // 남은 소수 처리
+    if (n > 2) {
         factors.push(n);
     }
     
