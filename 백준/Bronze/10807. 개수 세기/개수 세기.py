ab327e248e1b09955e4a138d1ab38a749e0b138d
전명환 <153697827+JMHman@users.noreply.github.com>
2024-08-09 11:33:52 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,21 +1,11 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
-numbers = list(map(int,sys.stdin.readline().strip().split()))
+numbers = list(map(int, sys.stdin.readline().strip().split()))
 v = int(sys.stdin.readline().strip())
 
-num_count = 0
-
-num_set = set(numbers)
-for a in num_set:
-    if a == v:
-        num_count += 1
-    
-if num_count == 0:
+if v in numbers:
+    num_count = sum(1 for number in numbers if number == v)
     print(num_count)
 else:
-    for i in numbers:
-        if i == v:
-            num_count+=1
-    print(num_count-1)
-        
+    print(0)
