29d89d09635fbec3fb1b33ee9ac0d8943358ce4c
전명환 <153697827+JMHman@users.noreply.github.com>
2024-08-15 19:24:40 +0900

    [Silver II] Title: 에디터, Time: 252 ms, Memory: 78872 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,30 +1,29 @@
 import sys
 
-# 초기 문자열 읽기
-initial_string = sys.stdin.readline().strip()
-# 명령어 개수 읽기
-command_count = int(sys.stdin.readline().strip())
+# 입력을 읽어 각 줄별로 처리
+input_edit = sys.stdin.read().splitlines()
+initial_str = input_edit[0]
+commands_count = int(input_edit[1])
 
-# 커서를 관리할 두 개의 스택 사용 (좌측 스택, 우측 스택)
-left_stack = list(initial_string)  # 커서 왼쪽의 모든 문자를 포함
-right_stack = []  # 커서 오른쪽의 문자는 초기에 비어 있음
+# 문자열을 리스트로 관리하고, 초기 커서 위치는 문자열의 끝
+left_stack = list(initial_str)  # 커서 왼쪽의 모든 문자
+right_stack = []  # 커서 오른쪽은 초기에 비어있음
 
-# 명령어 처리
-for _ in range(command_count):
-    command = sys.stdin.readline().strip().split()
+# 커맨드 처리
+for i in range(2, 2 + commands_count):
+    command = input_edit[i].strip().split()
     if command[0] == 'L':
-        if left_stack:  # 커서를 왼쪽으로 옮길 수 있으면
-            right_stack.append(left_stack.pop())
+        if left_stack:
+            right_stack.append(left_stack.pop())  # 왼쪽 스택에서 빼서 오른쪽 스택으로 이동
     elif command[0] == 'D':
-        if right_stack:  # 커서를 오른쪽으로 옮길 수 있으면
-            left_stack.append(right_stack.pop())
+        if right_stack:
+            left_stack.append(right_stack.pop())  # 오른쪽 스택에서 빼서 왼쪽 스택으로 이동
     elif command[0] == 'B':
-        if left_stack:  # 커서 왼쪽의 문자 삭제
-            left_stack.pop()
+        if left_stack:
+            left_stack.pop()  # 왼쪽 스택의 마지막 원소 제거
     elif command[0] == 'P':
-        left_stack.append(command[1])  # 커서 왼쪽에 문자 추가
+        left_stack.append(command[1])  # 왼쪽 스택에 새 문자 추가
 
-# 최종 문자열 구성
-# 왼쪽 스택은 그대로, 오른쪽 스택은 뒤집어서 합치기
-result = ''.join(left_stack + right_stack[::-1])
-print(result)
+# 최종 문자열 조합
+final_string = ''.join(left_stack + right_stack[::-1])
+print(final_string)
