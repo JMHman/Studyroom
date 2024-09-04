5b0d319e96acca7ec9f63b72c59ec40e1060a8d2
전명환 <153697827+JMHman@users.noreply.github.com>
2024-09-04 22:34:31 +0900

    [level 2] Title: 전화번호 목록, Time: 458.67 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,12 @@
 def solution(phone_book):
-    phone_book.sort()  # 전화번호를 사전 순으로 정렬
-    
-    for i in range(len(phone_book) - 1):
-        # 정렬된 목록에서 현재 번호가 다음 번호의 접두사인 경우
-        if phone_book[i + 1].startswith(phone_book[i]):
-            return False
-            
-    return True
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
